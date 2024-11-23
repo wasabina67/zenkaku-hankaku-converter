c86b8ea337ae474b1448897bd7e1d126126da35b
wasabina67 <wasabina67@gmail.com>
2024-11-23 16:15:03 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <div>
     <h1>zenkaku-hankaku-converter</h1>
     <h4>Convert alphanumeric characters only</h4>
+
+    <p>Text to convert</p>
+    <textarea placeholder="Enter your text"></textarea>
+
+    <p>zenkaku</p>
+    <textarea placeholder=""></textarea>
+
+    <p>hankaku</p>
+    <textarea placeholder=""></textarea>
   </div>
 </template>
 
@@ -21,5 +30,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+p {
+  margin-bottom: 5px;
+}
+
+textarea {
+  font-size: 1rem;
+  height: 50px;
+  width: 75%;
+}
+
+textarea:focus {
+  outline-color: #42b883;
 }
 </style>
